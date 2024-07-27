517c0af48ca64ed9b65eedc019f0dae7c899f856
Pawan Sharma <119160611+pawan-sharma3110@users.noreply.github.com>
2024-07-28 00:25:19 +0530

    Update main.go
    change local host port

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 
 	router.AllRoutes()
 
-	fmt.Println("Starting server at port 8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting server at port 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
