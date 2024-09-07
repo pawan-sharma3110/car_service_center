3410b9a802b888ef7b24ad23dab5ffa73b699b70
Pawan Sharma <pawansharma93520@gmail.com>
2024-09-08 04:01:19 +0530

    appointment ui left

--- backend/router/router.go
+++ backend/router/router.go
@@ -21,6 +21,6 @@ func AllRoutes() {
 	http.HandleFunc("/service/delete/{id}", handler.DeleteById)
 	http.HandleFunc("/service/delete/all", handler.AllServiceDelete)
 	http.HandleFunc("/service/update/{id}", handler.UpdadeService)
-	http.HandleFunc("/appointments", handler.GetAllAppointmentsHandler)
+	http.HandleFunc("/admin/appointments", handler.GetAllAppointmentsHandler)
 	http.HandleFunc("/admin/appointment/{id}", handler.AppointmentStatusUpdate)
 }
