080c7b24e0a5393a9e00aaa51c181e9c7e97889a
Pawan Sharma <pawansharma93520@gmail.com>
2024-09-12 23:13:35 +0530

    midlware added

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"car_service/handler"
+	"car_service/utils"
 	"net/http"
 )
 
@@ -12,7 +13,7 @@ func AllRoutes() {
 	http.HandleFunc("/user/login", handler.Login)
 
 	// Admin Services
-	http.HandleFunc("/users", handler.GetAllUser)
+	http.HandleFunc("/users", utils.RoleBasedAuth(handler.GetAllUser, "admin"))
 	http.HandleFunc("/user/delete/{id}", handler.DeleteUserById)
 	http.HandleFunc("/service/create", handler.CreateService)
 	http.HandleFunc("/service/search", handler.SearchServicesHandler)
