86f21d2f69e383e2eecb74a5e9dae9e7c57a1bd1
Pawan Sharma <pawansharma93520@gmail.com>
2024-09-13 02:39:06 +0530

    middleware set on all admin access endpoints

--- backend/router/router.go
+++ backend/router/router.go
@@ -11,19 +11,20 @@ func AllRoutes() {
 	http.Handle("/", fileServer)
 	http.HandleFunc("/user/register", handler.RegisterHandler)
 	http.HandleFunc("/user/login", handler.Login)
-
-	// Admin Services
-	http.HandleFunc("/users", middleware.RoleBasedAuth(handler.GetAllUser, "admin"))
-	http.HandleFunc("/user/delete/{id}", handler.DeleteUserById)
-	http.HandleFunc("/service/create", handler.CreateService)
 	http.HandleFunc("/service/search", handler.SearchServicesHandler)
 	http.HandleFunc("/service/get/all", handler.GetAllService)
-	http.HandleFunc("/service/get", handler.GetServiceByIDHandler)
-	http.HandleFunc("/service/delete/{id}", handler.DeleteById)
-	http.HandleFunc("/service/delete/all", handler.AllServiceDelete)
-	http.HandleFunc("/service/update/{id}", handler.UpdadeService)
 	http.HandleFunc("/appointment", handler.CreateAppointment)
-	http.HandleFunc("/appointment/{id}", handler.AppointmentByID)
-	http.HandleFunc("/appointments", handler.GetAppointments)
-	http.HandleFunc("/admin/appointment/{id}", handler.UpdateAppointment)
+	// Admin Services
+
+	
+	http.HandleFunc("/users", middleware.RoleBasedAuth(handler.GetAllUser, "admin"))
+	http.HandleFunc("/user/delete/{id}", middleware.RoleBasedAuth(handler.DeleteUserById, "admin"))
+	http.HandleFunc("/service/create", middleware.RoleBasedAuth(handler.CreateService, "admin"))
+	http.HandleFunc("/service/get", middleware.RoleBasedAuth(handler.GetServiceByIDHandler, "admin"))
+	http.HandleFunc("/service/delete/{id}", middleware.RoleBasedAuth(handler.DeleteById, "admin"))
+	http.HandleFunc("/service/delete/all", middleware.RoleBasedAuth(handler.AllServiceDelete, "admin"))
+	http.HandleFunc("/service/update/{id}", middleware.RoleBasedAuth(handler.UpdadeService, "admin"))
+	http.HandleFunc("/appointment/{id}", middleware.RoleBasedAuth(handler.AppointmentByID, "admin"))
+	http.HandleFunc("/appointments", middleware.RoleBasedAuth(handler.GetAppointments, "admin"))
+	http.HandleFunc("/admin/appointment/{id}", middleware.RoleBasedAuth(handler.UpdateAppointment))
 }
